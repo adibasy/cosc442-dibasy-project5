3ebe1bb6b56d15441b3f0761ff8c9a240aeae266
Abdulai <dibasy36@gmail.com>
2018-04-26 13:21:10 -0400

    Update jamesBond.java

--- Task3/Project5/src/Project5/jamesBond.java
+++ Task3/Project5/src/Project5/jamesBond.java
@@ -7,7 +7,6 @@ public class jamesBond {
 public Boolean bondRegex(String input) {
 		
 		Pattern p = Pattern.compile("([^\\(]\\s).*\\(\\s([^0]\\s).*(0\\s){2}7\\s([^\\)])");
-				//".*[\\(].*(0{2})[7].*[\\)]");
 		Matcher m = p.matcher(input);
 		return m.find();
 		
